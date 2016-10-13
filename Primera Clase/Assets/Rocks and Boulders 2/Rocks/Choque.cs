61c1c5510486f7e6a856ab6705db051dddf51324
alu0100811933 <alu0100811933@ull.edu.es>
2016-10-13 19:53:41 +0100

    Ficheros

---
+++ Primera Clase/Assets/Rocks and Boulders 2/Rocks/Choque.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Choque : MonoBehaviour {
+	public delegate void cambiarColor ();
+
+	public static cambiarColor CambiarC;
+
+	void OnCollisionEnter (Collision col) {
+		if (col.gameObject.tag == "Transformer") {
+			CambiarC();
+		}
+			
+}
+}
---
+++ Primera Clase/Assets/Rocks and Boulders 2/Rocks/RockCollider.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RockCollider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		Choque.CambiarC += Cambio;
+	}
+
+	void Cambio() {
+	// Destroy (this.gameObject);
+		this.gameObject.transform.Rotate(Vector3.up, 10, Space.World);
+
+		Color newColor = new Color (Random.value, Random.value, Random.value);
+
+		this.gameObject.GetComponent<Renderer> ().material.color = newColor;
+		//GetComponent<Renderer>().material.color = Color.white;
+	}
+	// Update is called once per frame
+	void Update () {
+	}
+}
